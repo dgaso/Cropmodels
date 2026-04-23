4e1f6755266535127ddb76e2061d244f2f552306
Deborah Gaso <dgaso@inia.org.uy>
2026-04-23 12:00:06 -0300

    soil.py

--- campbell_diaz/soil.py
+++ campbell_diaz/soil.py
@@ -45,8 +45,8 @@ class Water_balance(SimulationObject):
     PWPP         Permanent wilting point                            mH2O m Soi   
     ADWCP         Air dry water content                              mH2O m Soi   
     TCK          Thickness of soil layer                                    m
-    RUNOFF1      Parameter 1 for runoff function   
-    RUNOFF2      Parameter 2 for runoff function          
+    RUNOFF1      Parameter 1 for runoff function                        -
+    RUNOFF2      Parameter 2 for runoff function                        -
     RMIN         Root resistance                                         J kg-1 
     PSIPWP       Permanent wilting water potential                       J kg-1 
     PSIFC        Field capacity water potential                          J kg-1 
@@ -59,40 +59,49 @@ class Water_balance(SimulationObject):
     ============ ================================================= ==== ========
      Name        Description                                             Unit
     ============ ================================================= ==== ========
-    FC           Parameter FCP in m of H2O per layer                       m
-    PWP          Parameter PWPP in m of H2O per layer                      m
     INTERC       Precipitation intercepted by the canopy                    m
+    GPREC        Ground precipitation                                       m
     RUNOFF       Rate of runoff                                             m
     INFIL        Water available for infiltration                           m
+    EVS          Rate of soil evaporation                                   m
+    TL           Transpiration per layer                                    m
+    AT           Actual Evapotranspiration per layer                        m
+    T            Total Evapotranspiration                                   m
+    net_RW	     Net water recharge                                         m
+    ============ ================================================= ==== ========
+    Intermediate variables within Rates function**
+
+    ============ ================================================= ==== ========
+     Name        Description                                             Unit
+    ============ ================================================= ==== ========
     RW           Rate of recharging water in each layer                     m
     NWC          Rate of water in the 1st layer                             m
-    EVS          Rate of soil evaporation                                   m
     RDr          Rate of root growth                                        m
     B            Value for power equation of soil potential
     A            Value for power equation of soil potential
     SPSI         Soil potential per layer                            J kg-1 m-1
-    FROOT        Root fraction per layer
+    FROOT        Root fraction per layer                                   -
     AVESPSI      Soil potential weighten by rooting fraction         J kg-1 m-1
     RBAR         Root resistance                                     J kg-1 m-1
     PSIX         Xilem Potential                                     J kg-1 m-1
     LOSS         Rate of water loss per layer                            mH2O m
-    TL           Transpiration per layer                                    m
-    AT           Actual Evapotranspiration per layer                        m
-    T            Total Evapotranspiration                                   m
-   
-    ============ ================================================= ==== ========
+       
+    ============ ================================================= ==== ========   
+
+ ============ ================================================= ==== ========
     State variables**
  
     Name         Description                                             Unit
     ============ ================================================= ==== ========
     TINTERC      Total interception by the canopy                           m
-    GPREC        Ground precipitation                                       m
     TRUNOFF      Total runoff                                               m
-    PERC         Total deep percolation                                     m
+    PERC         Total deep percolation                                     m   
+    TE           Total soil evaporation                                     m
+    WC           Water content per layer                                    m
+    WCv          Volumetric water content per layer                     mH2O m Soil
+    TT           Total transpiration                                        m
+    WB_close     Check water balance                                        m
     TWC          Water content                                          m-3 m-3
-    TEVS         Total soil evaporation                                     m
-    TRD          Root depth                                                 m
-    TFR          Root fraction in each layer 
                                     
     ============ ================================================= ==== ========
     **External dependencies:**
@@ -110,7 +119,7 @@ class Water_balance(SimulationObject):
     pheno = Instance(SimulationObject)
 
     class Parameters(ParamTemplate):                        
-        #Soil water balance
+        
         FCP    = Float(-99.)
         PWPP   = Float(-99.)
         ADWCP  = Float(-99.)
@@ -120,95 +129,58 @@ class Water_balance(SimulationObject):
         RMIN  = Float(-99.)
         PSIPWP= Float(-99.)
         PSIFC = Float(-99.)
-        S     = Float(-99.)
-        
+        S     = Float(-99.)        
         RDMAX = Float(-99.)
         
     class RateVariables(RatesTemplate):
+                    
         PE        = Float(-99.)
         PT        = Float(-99.)
-        ETP        = Float(-99.)
-        PREC      = Float(-99.)
-        nl        = Int
-        FC        = Float(-99.) 
-        PWP       = Float(-99.) 
-        ADWC      = Float(-99.)
-        
-        B         = Float(-99.)
-        A         = Float(-99.)        
         INTERC    = Float(-99.)        
         GPREC     = Float(-99.)        
         RUNOFF    = Float(-99.)
         INFIL     = Float(-99.)        
-        RW        = Float(-99.)        
-        values_RW = [] 
-        RWATER    = Instance(np.ndarray)         
-        NWC       = Float(-99.)
-        EVS       = Float(-99.)
-                            
-        SPSI      = Float(-99.)
-        values_SPSI = []
-        SP        = Instance(np.ndarray) 
-        #root
-        FcR       = Instance(np.ndarray) 
-        values_FR = []
-        FROOT     = Float(-99.)
-        AVEPSI    = Float(-99.)
-        RBAR      = Float(-99.)
-        PSIX      = Float(-99.) 
-        z         = Float(-99.) 
-
-        LOSS      = Instance(np.ndarray) 
-        W         = Instance(np.ndarray)                                  
-        arr_bool  = Instance(np.ndarray) 
-        arr_TRUE  = Instance(np.ndarray) 
-        TL        = Instance(np.ndarray) 
-        net_RW    = Instance(np.ndarray) 
+        EVS       = Float(-99.)                           
         T         = Float(-99.)
-        arr_subt  = Instance(np.ndarray) 
-        arr_mult  = Instance(np.ndarray) 
-        AT         = Instance(np.ndarray) 
-        check = Float(-99.)
-
+        TL        = Instance(np.ndarray) 
+        AT        = Instance(np.ndarray) 
+        net_RW    = Instance(np.ndarray)
+        
     class StateVariables(StatesTemplate):  
         PTa     = Float(-99.)
+        Ta      = Float(-99.)   
+        Tp      = Float(-99.)
         #Soil water balance
-        TPE     = Float(-99.)
-        TPT     = Float(-99.)
-        TPREC   = Float(-99.)
+        WC      = Instance(np.ndarray) 
+        WCv     = Instance(np.ndarray)                    
         TINTERC = Float(-99.)
         TRUNOFF = Float(-99.)         
-        PERC    = Float(-99.)                
-        TE      = Float(-99.)
-        Ta      = Float(-99.)    
-        WC      = Instance(np.ndarray) 
-        WCv     = Instance(np.ndarray) 
-        #To chech WB closed
+        PERC    = Float(-99.)                      
         TT        = Float(-99.)
+        TE        = Float(-99.)
         WB_close  = Float(-99.)
-        Diff_WC   = Float(-99.)
         TWC       = Float(-99.)
         W_Stress  = Float(-99.)
         
-        # Cumulaive variables to extract features
-        
+        # Cumulaive variables to extract features      
         CRainv = Float(-99.)
         CRainr = Float(-99.)
         TWCR1 = Float(-99.)
         TWCR5 = Float(-99.)
                 
     def initialize(self, day, kiosk, parametervalues):        
-        self.params = self.Parameters(parametervalues)
-        self.rates = self.RateVariables(kiosk,publish = None)        
-        self.kiosk = kiosk
+        self.params = self.Parameters(parametervalues)         
+        self.rates = self.RateVariables(kiosk,publish = ["T","PT"]) 
+        self.kiosk = kiosk        
         layers = math.floor(self.params.RDMAX/self.params.TCK)
-        self.states = self.StateVariables(kiosk, publish=["Ta", "W_Stress","PTa"],  PTa=0,
-                                          TPE=0.0, TPT=0.0,
+        self.states = self.StateVariables(kiosk, publish=["Ta", "W_Stress","PTa"], Ta=0, PTa=0, Tp=0,                                        
                                           WC=np.full(layers,self.params.FCP*self.params.TCK), 
                                           WCv=np.full(layers,self.params.FCP*self.params.TCK),
-                                          TT=0.0, Diff_WC=0., W_Stress=0, TINTERC=0, TRUNOFF=0,
-                                          PERC=0, TE=0, Ta=0,  WB_close =0, TPREC=0.0, TWC=0.,
+                                          TT=0.0, Diff_WC=0., W_Stress=1, TINTERC=0, TRUNOFF=0,
+                                          PERC=0, TE=0, WB_close =0,  
+                                          TWC=layers * ((self.params.FCP - self.params.PWPP) * self.params.TCK) * 1000.0,
                                           CRainv =0., CRainr = 0.,TWCR1 = None, TWCR5 = None)
+
 
     @prepare_rates
     def calc_rates(self, day, drv):                
@@ -229,22 +201,14 @@ class Water_balance(SimulationObject):
 
         r.PE = convert_cm_to_m(drv.ET0 * (1 - k.FI))
         r.PT = convert_cm_to_m(drv.ET0 * k.FI)
-        r.ETP=convert_cm_to_m(drv.ET0)*100*10
-       
-       
+                   
         # Convert fc and pwp to m of H2O
-        r.nl = math.floor(p.RDMAX/p.TCK) 
-        r.FC = p.FCP * p.TCK
-        r.PWP = p.PWPP * p.TCK
-        r.ADWC = p.ADWCP * p.TCK
         
-        # Parameters for eq os SPSI
-        r.B = math.log(p.PSIPWP/p.PSIFC)/math.log(p.FCP/p.PWPP)
-        r.A = math.exp((math.log(-p.PSIFC)) + r.B * math.log(max(p.FCP , sys.float_info.min)) )
-
+        FC = p.FCP * p.TCK
+        PWP = p.PWPP * p.TCK
+        ADWC = p.ADWCP * p.TCK
+               
         # Interception
-        r.PREC = drv.RAIN /100
-       
         if drv.RAIN != 0.:
             r.INTERC = min(drv.RAIN/100, 0.001 * k.FI) 
         r.GPREC = ((drv.RAIN/100) - r.INTERC)
@@ -256,75 +220,93 @@ class Water_balance(SimulationObject):
             r.RUNOFF = ((r.GPREC - p.RUNOFF1 * p.S)**2)/(r.GPREC + p.RUNOFF2 * p.S)                
         r.INFIL = ((drv.RAIN/100) - r.INTERC - r.RUNOFF)
 
+        # Parameters for eq os SPSI
+        B = math.log(p.PSIPWP/p.PSIFC)/math.log(p.FCP/p.PWPP)
+        A = math.exp((math.log(-p.PSIFC)) + B * math.log(max(p.FCP , sys.float_info.min)) )
+        AVEPSI = 0  
+        z=0     
+        
         # Recharge water per layer
-        r.values_RW = []
-        r.values_FR = []
-        r.values_SPSI = []
+        values_RW = []
+        values_FR = []
+        values_SPSI = []              
         for j in range (s.WC.shape[0]):
             if r.INFIL>0:
-                if r.INFIL <= (r.FC - s.WC[j]):
-                    r.RW = r.INFIL
+                if r.INFIL <= (FC - s.WC[j]):
+                    RW = r.INFIL
                     r.INFIL = 0.
                 else:                 
-                    r.RW = r.FC - s.WC[j] 
-                    r.INFIL =  r.INFIL - (r.FC - s.WC[j])    
+                    RW = FC - s.WC[j] 
+                    r.INFIL =  r.INFIL - (FC - s.WC[j])    
             else: 
-                r.RW = 0                
+                RW = 0                
                             
-            r.values_RW.append(r.RW)         
-            r.RWATER = np.array(r.values_RW)  
+            values_RW.append(RW)         
+            RWATER = np.array(values_RW)  
             
             # Evaporation calculation
             if j < 1:
-                if s.WC[0]>r.PWP:
+                if s.WC[0]>PWP:
                     r.EVS = r.PE                  
-                if s.WC[0] < r.PWP:
-                    r.EVS = r.PE * (((s.WC[0]/p.TCK)-p.ADWCP)/(p.PWPP - p.ADWCP))**2.                 
-                r.NWC = s.WC[0] - r.EVS
-                             
-                if r.NWC < r.ADWC: 
-                    r.NWC = r.ADWC                    
-                 
+                else:                    
+                    evapo = r.PE * (((s.WC[0]/p.TCK)-p.ADWCP)/(p.PWPP - p.ADWCP))**2. 
+                    r.EVS = min(evapo, 1e6) 
                                            
-            # Fraction root per layer
-            if j >= 1:
-                r.z+=p.TCK
-                if r.z <= k.TRD:                     
-                    r.FROOT = p.TCK*(2.*(k.TRD - r.z) + p.TCK)/(k.TRD*k.TRD)
-                elif r.z > k.TRD and (r.z - p.TCK) < k.TRD:
-                    r.FROOT = ((k.TRD - r.z + p.TCK)/k.TRD)**2.
+                NWC = s.WC[0] - r.EVS
+                             
+                if NWC < ADWC: 
+                    NWC = ADWC                    
+                    s.WC[0]= NWC
+                                           
+            # Fraction root per layer           
+            if j >= 1:               
+                z+=p.TCK
+                if z <= k.TRD:                     
+                    FROOT = p.TCK*(2.*(k.TRD - z) + p.TCK)/(k.TRD*k.TRD)
+                elif z > k.TRD and (z - p.TCK) < k.TRD:
+                    FROOT = ((k.TRD - z + p.TCK)/k.TRD)**2.
                 else:
-                    r.FROOT = 0.
+                    FROOT = 0.
 
-                r.SPSI = -r.A*mp.exp(-r.B*math.log(max((s.WC[j]/p.TCK), sys.float_info.min)))
-                r.AVEPSI += (r.FROOT*r.SPSI)                                      
-                r.values_FR.append(r.FROOT)  
-                r.FcR = np.array(r.values_FR)
-                r.values_SPSI.append(r.SPSI)
-                r.SP = np.array(r.values_SPSI)
-
-        r.RBAR = p.RMIN/max(k.FI, 1e-70)
-        r.PSIX = r.AVEPSI - (r.RBAR*r.PT) 
-    
-        if r.PSIX < p.PSIPWP: r.PSIX=p.PSIPWP
-       
-        # Transpiration calculation
-        r.arr_subt = np.subtract(r.SP, r.PSIX)
-        r.arr_mult = np.multiply(r.FcR, r.arr_subt)
-        r.LOSS = np.true_divide(r.arr_mult, (r.RBAR*p.TCK))
-     
-        # check loss under water deficit
-        r.W = np.true_divide(s.WC[1::], p.TCK)
-        r.arr_bool = (np.subtract(r.W, r.LOSS)) < p.PWPP
-        r.arr_TRUE = np.subtract(r.W, p.PWPP)
-        
-        # Actual evapotranspiration        
-        r.TL= np.multiply(np.where(r.arr_bool, r.arr_TRUE, r.LOSS), p.TCK)
-        r.AT = np.append(r.EVS, r.TL)
-        r.T = np.sum(r.TL)
-        r.net_RW = np.subtract(r.RWATER, r.AT)
-        
+                # Accumualte
+                SPSI = -A*mp.exp(-B*math.log(max((s.WC[j]/p.TCK), sys.float_info.min)))    
+                AVEPSI += (FROOT*SPSI)  
                 
+                values_FR.append(FROOT)  
+                FcR = np.array(values_FR)
+                values_SPSI.append(SPSI)
+                SP = np.array(values_SPSI)
+
+        
+        if k.FI <= 0.0:           
+            RBAR = np.inf
+            PSIX = p.PSIPWP
+            LOSS = np.zeros_like(FcR, dtype=float)
+            r.TL = np.zeros_like(FcR, dtype=float)
+        else:   
+            #RBAR = p.RMIN/max(k.FI, 1e-70)
+            RBAR = p.RMIN / k.FI
+            PSIX = AVEPSI - (RBAR*r.PT) 
+        
+            if PSIX < p.PSIPWP: PSIX=p.PSIPWP
+           
+            # Transpiration (LOSS) calculation
+            arr_subt = np.subtract(SP, PSIX)
+            arr_mult = np.multiply(FcR, arr_subt)
+            LOSS = np.true_divide(arr_mult, (RBAR*p.TCK))
+         
+            # check loss under water deficit
+            W = np.true_divide(s.WC[1::], p.TCK) #convert water content in m to volumentric
+            arr_bool = (np.subtract(W, LOSS)) < p.PWPP
+            arr_TRUE = np.subtract(W, p.PWPP)
+            
+            # Actual evapotranspiration        
+            r.TL= np.multiply(np.where(arr_bool, arr_TRUE, LOSS), p.TCK)
+            r.AT = np.append(r.EVS, r.TL)
+            r.T = np.sum(r.TL)
+            r.net_RW = np.subtract(RWATER, r.AT)
+            
+           
 
     @prepare_states
     def integrate(self, day,delt):
@@ -333,33 +315,41 @@ class Water_balance(SimulationObject):
         s = self.states
         k = self.kiosk
 
-        s.PTa = r.PT                                                      
-        # Soil water balance
-        s.TPE += r.PE
-        s.TPT = r.PT                                        
-        s.TPREC=r.PREC
+        nl = math.floor(p.RDMAX/p.TCK)
+        FC = p.FCP * p.TCK
+        PWP = p.PWPP * p.TCK   
+        
+        s.PTa = r.PT    
+        s.Ta = r.T
+        s.Tp = r.PT
+        
+        # Soil water balance  
         s.TINTERC +=  r.GPREC
         s.TRUNOFF +=  r.RUNOFF
         s.PERC +=r.INFIL        
-        s.TE += r.EVS
-        s.Ta = r.T
-        
+        s.TE += r.EVS      
         s.WC = np.add(s.WC, r.net_RW)
         s.WCv = (s.WC / p.TCK)    
           
         # check WB closed
         s.TT+=r.T
-        s.Diff_WC = np.sum(np.subtract(np.full(r.nl, r.FC), s.WC) )
-        s.WB_close = s.TINTERC - s.TRUNOFF - s.PERC - s.TT - s.TE + s.Diff_WC
-        s.TWC = (np.sum(np.subtract(s.WC, r.PWP)))*1000
-        s.W_Stress = r.T / max(r.PT, 1e-70)
+        Diff_WC = np.sum(np.subtract(np.full(nl, FC), s.WC) )
+        s.WB_close = s.TINTERC - s.TRUNOFF - s.PERC - s.TT - s.TE + Diff_WC
+        s.TWC = (np.sum(np.subtract(s.WC, PWP)))*1000
+        
+        if r.PT <= 0.0 or k.TRD <= 0.0:
+            s.W_Stress = 1.0
+        else:
+            s.W_Stress = r.T / r.PT
+            
+        #s.W_Stress = r.T / max(r.PT, 1e-70)
         
         if k.DVS > 0 and k.DVS <= 1.:
             s.CRainv += r.GPREC * 1000
                
         if k.DVS > 1 and k.DVS <= 2.:
             s.CRainr += r.GPREC * 1000
-           
+       
         if s.TWCR1 is None and k.DVS >= 1.:
             s.TWCR1 = max(0,s.TWC)
             
